6839f5e69aaa5f02e9780123dcc2940f1a46ca4c
Samindi21 <shashikasamindi1221@gmail.com>
2024-01-08 21:55:14 +0530

    added functions

---
+++ tests/registerTest.spec.ts
@@ -0,0 +1,52 @@
+/*
+1)go to url and loads the homepage
+2)click 'Login/Register' 
+3)validate the heading 'log into Wiley Online Library' is displayed or not
+4)click email textbox
+5)Enter email
+6)click password textbox
+7)enter your password
+8)click 'Log In' button
+
+*/
+
+const {test , expect} = require ('@playwright/test');
+
+test.only('Register Page', async ({page})=> {
+
+    await page.goto('https://onlinelibrary.wiley.com'); //continue keep throughtout alive
+
+    //await page.getByRole('label',{class:'sign-in-label'}).click();
+
+    const linkText = 'Login / Register'; // Replace with the actual text of the link you want to click
+    await page.click(`text=${linkText}`);
+
+    //validate the heading
+    //const headingText = await page.textContent()
+    const headingText = 'Log in to Wiley Online Library';
+    await expect(page.getByText(headingText)).toBeVisible();
+
+
+    // Fill in the username and password fields
+    await page.type('input[id="username"]','shashikasamindi1221@gmail.com');
+    await page.type('input[id="password"]', 'ShashiS');
+    
+    await page.waitForTimeout(2000);
+
+
+    await page.click('#pb-page-content > div > div.popup.login-popup > div > div.modal__body > div.login-form > form > div.align-end > span > input');
+    
+    await page.waitForTimeout(3000);
+
+
+    await page.close();
+
+} )
+
+//in async every step processed with the previous one
+//async keyword make function returns a promise.
+//await makes the function wait for the promise. 
+
+
+
+
---
+++ tests/subjectSelectionTest.spec.ts
@@ -0,0 +1,24 @@
+/*
+
+1)go to url and open the page
+2)click subject bar of 'Computer science and information technology'
+3)click on computer science
+4)check whether the page is redirected to the computer science page
+5)check the heading "Computer Science" is displayed.
+
+*/
+
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://onlinelibrary.wiley.com');
+
+
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+
+
+  await page.close();
+
+});
